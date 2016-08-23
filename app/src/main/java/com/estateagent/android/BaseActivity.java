b1a42ae2477c0a477746cf7d5ad71ef6f132c707
Mjh910101 <408951390@qq.com>
2016-08-23 11:45:02 +0800

    test

---
+++ app/src/main/java/com/estateagent/android/BaseActivity.java
@@ -0,0 +1,38 @@
+package com.estateagent.android;
+
+import android.app.Activity;
+import android.content.Context;
+import android.os.Bundle;
+
+/**
+ * *
+ * * ┏┓      ┏┓
+ * *┏┛┻━━━━━━┛┻┓
+ * *┃          ┃
+ * *┃          ┃
+ * *┃ ┳┛   ┗┳  ┃
+ * *┃          ┃
+ * *┃    ┻     ┃
+ * *┃          ┃
+ * *┗━┓      ┏━┛
+ * *  ┃      ┃
+ * *  ┃      ┃
+ * *  ┃      ┗━━━┓
+ * *  ┃          ┣┓
+ * *  ┃         ┏┛
+ * *  ┗┓┓┏━━━┳┓┏┛
+ * *   ┃┫┫   ┃┫┫
+ * *   ┗┻┛   ┗┻┛
+ * Created by Hua on 16/8/23.
+ */
+public class BaseActivity extends Activity {
+
+    Context context;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        context = this;
+    }
+
+}
